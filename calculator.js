b65a2b5f979f1a4a9e56e2e695de0c9cc449680e
ArsenyKutepov <91091650+ArsenyKutepov@users.noreply.github.com>
2021-12-08 10:11:23 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,15 +1,8 @@
-window.addEventListener("DOMContentLoaded", function(event)  {
-    console.log("DOM loaded ^_^");
-
-    let buttonProiz = document.getElementById("proiz");
-    buttonProiz.addEventListener("click",proiz);
-});
-
 function proiz(){
-    check = /^-+\d+$|\d+$/;
+     check = /^-+\d+$|\d+$/;
     let num1 = document.getElementsByName("n1");
     let num2 = document.getElementsByName("n2");
-
+    
     if (check.test(num1[0].value) == false|| check.test(num2[0].value) == false) {
         document.getElementById('out').innerHTML='Вы сделали что-то не так!';
     }
@@ -17,9 +10,15 @@ function proiz(){
         let num3 = document.getElementById("out");
         num1[0] = Number.parseInt(num1[0]);
         num2[0] = Number.parseInt(num2[0]);
-        var result = num1[0].value * num2[0].value;
-        num3.innerHTML = Math.abs(result);
+         result = num1[0].value * num2[0].value;
+        num3.innerHTML = Math.abs(result);   
     }
 }
 
+    
+window.addEventListener("DOMContentLoaded", function(event)  {
+    console.log("DOM loaded ^_^");
 
+    let buttonProiz = document.getElementById("proiz");
+    buttonProiz.addEventListener("Click",proiz);
+})
